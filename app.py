ae075456d8bbcd17f4bc1bdf8170e2d2bae7ffac
ranjanyadavgit <59697752+ranjanyadavgit@users.noreply.github.com>
2025-06-27 22:34:55 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import logging
+app = Flask(__name__)
+# Configure logging
+logging.basicConfig(filename='app.log', level=logging.INFO)
+@app.route("/")
+def home():
+ app.logger.info("Home route accessed")
+ return "Monitoring Docker App"
+17
+if __name__ == "__main__":
+ app.run(host="0.0.0.0", port=5000)
